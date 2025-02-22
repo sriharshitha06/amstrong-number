8012358f35c01ce2198cd2ec1b26255ed44eebbe
MANTHENA SRI HARSHITHA <manthenasiri23@gmail.com>
2025-02-22 22:57:38 +0530

    Update ams_num2.cpp

--- ams_num2.cpp
+++ ams_num2.cpp
@@ -1,1 +1,24 @@
-
+#include <iostream>
+#include <string> 
+#include <cmath>  
+using namespace std;
+bool isArmstrong(int num) {
+    string numStr = to_string(num); 
+    int n = numStr.length();       
+    int sum = 0;
+    for (char ch : numStr) {
+        int digit = ch - '0';        
+        sum += pow(digit, n);          
+    }
+    return sum == num;
+}
+int main() {
+    int num;
+    cin >> num;
+    if (isArmstrong(num)) {
+        cout << num << " is an Armstrong number." << endl;
+    } else {
+        cout << num << " is not an Armstrong number." << endl;
+    }
+    return 0;
+}
