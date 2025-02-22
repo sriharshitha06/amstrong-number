8765f3872a8d6e0b2ace5d4d2e80dbef06972ec8
MANTHENA SRI HARSHITHA <manthenasiri23@gmail.com>
2025-02-23 00:16:15 +0530

    Update ams_num1.cpp

--- ams_num1.cpp
+++ ams_num1.cpp
@@ -1,25 +1,32 @@
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
+
+// Check if num is an Armstrong number
 bool isArmstrong(int num) {
-    int k = to_string(num).length();
-    int sum = 0;
-    int n = num;
-    while(n > 0){
-        int ld = n % 10;
-        sum += pow(ld, k); 
-        n = n / 10;
+    int k = to_string(num).length(); // Number of digits
+    int sum = 0; // Sum of powered digits
+    int n = num; // Copy of the original number
+
+    // Loop through each digit
+    while (n > 0) {
+        int ld = n % 10; // Get last digit
+        sum += pow(ld, k); // Add powered last digit
+        n = n / 10; // Remove last digit
     }
-    return sum == num ? true : false;
+    
+    return sum == num; // Check if sum equals original number
 }
+
 int main() {
-    int n;
-  cin>>n;
+    int n; 
+    cin >> n; 
+
+    // Check and print if n is an Armstrong number
     if (isArmstrong(n)) {
-        cout << n << " is an Armstrong number." << endl;
+        cout << n << " is an Armstrong number." << endl; 
     } else {
-        cout << n << " is not an Armstrong number." << endl;
+        cout << n << " is not an Armstrong number." << endl; 
     }
-    return 0;
+    
+    return 0; 
 }
-                            
-                        
